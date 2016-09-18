239101aa976a71df899762ba9cdc0f8a3e2d8da3
Richard Abbott <richie0415@hotmail.com>
2016-09-18 16:27:14 -0400

    A little fix

--- SuperIn/src/net/keabotstudios/superin/Input.java
+++ SuperIn/src/net/keabotstudios/superin/Input.java
@@ -7,7 +7,6 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.nio.file.attribute.UserPrincipalLookupService;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
